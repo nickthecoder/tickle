8b0ddf7afe419a846264cc8357b957c503c8c1c3
Nick Robinson <nickthecoder@gmail.com>
2017-11-17 13:19:19 +0000

    Oops. Missed a new file from the previous commit.

---
+++ src/main/kotlin/uk/co/nickthecoder/tickle/util/Button.kt
@@ -0,0 +1,47 @@
+package uk.co.nickthecoder.tickle.util
+
+import uk.co.nickthecoder.tickle.AbstractRole
+import uk.co.nickthecoder.tickle.events.ButtonState
+import uk.co.nickthecoder.tickle.events.MouseEvent
+import uk.co.nickthecoder.tickle.events.MouseHandler
+
+abstract class Button : AbstractRole(), MouseHandler {
+
+    var down: Boolean = false
+        set(v) {
+            if (v != field) {
+                field = v
+                stateChanged(v)
+            }
+        }
+
+    override fun onMouseButton(event: MouseEvent) {
+        if (event.state == ButtonState.PRESSED) {
+            event.capture()
+            onMousePressed(event)
+        } else if (event.state == ButtonState.RELEASED) {
+            event.release()
+            onMouseReleased(event)
+        }
+    }
+
+    override fun onMouseMove(event: MouseEvent) {
+        down = actor.touching(event.viewPosition)
+    }
+
+    open fun onMousePressed(event: MouseEvent) {
+        down = true
+    }
+
+    open fun onMouseReleased(event: MouseEvent) {
+        if (down) {
+            onMouseClicked(event)
+        }
+        down = false
+    }
+
+    open fun onMouseClicked(event: MouseEvent) {}
+
+    open fun stateChanged(down: Boolean) {}
+
+}
