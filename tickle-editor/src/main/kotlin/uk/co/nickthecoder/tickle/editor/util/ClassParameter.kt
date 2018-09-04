6f1733e60f5a46e24952f549e28c598399a09ddb
Nick Robinson <nickthecoder@gmail.com>
2018-09-04 03:40:31 +0100

    ClassParameter is now a subtype of SimpleGroupParameter, rather than CompoundParameter (because it is simpler).

--- tickle-editor/src/main/kotlin/uk/co/nickthecoder/tickle/editor/util/ClassParameter.kt
+++ tickle-editor/src/main/kotlin/uk/co/nickthecoder/tickle/editor/util/ClassParameter.kt
@@ -1,7 +1,6 @@
 package uk.co.nickthecoder.tickle.editor.util
 
 import javafx.stage.Stage
-import javafx.util.StringConverter
 import uk.co.nickthecoder.paratask.gui.TaskPrompter
 import uk.co.nickthecoder.paratask.parameters.*
 import uk.co.nickthecoder.paratask.util.uncamel
@@ -16,15 +15,13 @@ class ClassParameter(
         required: Boolean = false,
         value: Class<*>? = null)
 
-    : CompoundParameter<Class<*>?>(name, label = label), ParameterListener {
+    : SimpleGroupParameter(name, label = label) {
 
     private val classP = GroupedChoiceParameter<Class<*>?>("${name}Class", required = required, value = null, allowSingleItemSubMenus = true)
 
     private val newButtonP = ButtonParameter("${name}New", buttonText = "New ${type.simpleName}") { newScript() }
 
-    override val converter: StringConverter<Class<*>?> = classP.converter
-
-    override var value: Class<*>?
+    var value: Class<*>?
         get() = classP.value
         set(value) {
             classP.value = value
@@ -37,12 +34,6 @@ class ClassParameter(
 
         newButtonP.hidden = ScriptManager.languages().isEmpty()
         ClassLister.setNullableChoices(classP, type)
-
-        classP.parameterListeners.add(this)
-    }
-
-    override fun parameterChanged(event: ParameterEvent) {
-        parameterListeners.fireValueChanged(this, event.oldValue)
     }
 
     private fun newScript() {
