bfc3641ad53c499e4b0bac3357145f78e9736152
Nick Robinson <nickthecoder@gmail.com>
2018-09-28 19:41:00 +0100

    CodeEditor can now automate adding import statements

--- tickle-editor/src/main/kotlin/uk/co/nickthecoder/tickle/editor/util/CodeEditor.kt
+++ tickle-editor/src/main/kotlin/uk/co/nickthecoder/tickle/editor/util/CodeEditor.kt
@@ -1,7 +1,12 @@
 package uk.co.nickthecoder.tickle.editor.util
 
+import javafx.event.EventHandler
+import javafx.scene.control.ContextMenu
+import javafx.scene.control.MenuItem
+import javafx.scene.control.SeparatorMenuItem
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyEvent
+import javafx.scene.input.MouseEvent
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.VBox
 import uk.co.nickthecoder.tedi.SourceCodeWordIterator
@@ -13,6 +18,7 @@ import uk.co.nickthecoder.tedi.ui.RemoveHiddenChildren
 import uk.co.nickthecoder.tedi.ui.ReplaceBar
 import uk.co.nickthecoder.tedi.ui.TextInputControlMatcher
 import java.io.File
+import java.util.regex.Pattern
 
 class CodeEditor {
 
@@ -50,6 +56,8 @@ class CodeEditor {
         }
 
         borderPane.addEventFilter(KeyEvent.KEY_PRESSED) { onKeyPressed(it) }
+        tediArea.addEventFilter(MouseEvent.MOUSE_PRESSED) { onMousePressedRelease(it) }
+        tediArea.addEventFilter(MouseEvent.MOUSE_RELEASED) { onMousePressedRelease(it) }
     }
 
     fun load(file: File) {
@@ -58,6 +66,97 @@ class CodeEditor {
 
     fun save(file: File) {
         file.writeText(tediArea.text)
+    }
+
+    fun onMousePressedRelease(event: MouseEvent) {
+        if (event.isPopupTrigger) {
+            tediArea.contextMenu = buildContextMenu(event)
+            tediArea.contextMenu?.let {
+                tediArea.contextMenu.show(tediArea, event.screenX, event.screenY)
+                event.consume()
+            }
+        }
+    }
+
+    fun buildContextMenu(event: MouseEvent): ContextMenu? {
+        val menu = ContextMenu()
+        var addSeparator = false
+        fun addItem(item: MenuItem) {
+            if (addSeparator && menu.items.isNotEmpty()) {
+                menu.items.add(SeparatorMenuItem())
+            }
+            menu.items.add(item)
+        }
+
+        val word = wordAt(event)
+        if (word != null) {
+            val classNames = findClasses(word)
+            classNames.filter { !isImported(it) }.forEach { className ->
+                val item2 = MenuItem("Import $className")
+                item2.onAction = EventHandler { addImport(className) }
+                addItem(item2)
+
+                val pack = packageName(className)
+                val item1 = MenuItem("Import ${pack}.*")
+                item1.onAction = EventHandler { addImport("${pack}.*") }
+                addItem(item1)
+            }
+            if (classNames.size > 1) addSeparator = true
+        }
+        return if (menu.items.isEmpty()) null else menu
+    }
+
+    fun isImported(className: String): Boolean {
+        val im = importPattern.matcher(tediArea.text)
+        while (im.find()) {
+            val imp = im.group("IMPORT")
+            if (imp == className || imp == "${packageName(className)}.*") {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun packageName(className: String) = className.substring(0, className.lastIndexOf('.'))
+
+    fun findClasses(word: String): List<String> {
+
+        return ClassMetaData.simpleClassNameToNames[word] ?: emptyList()
+    }
+
+    fun addImport(imp: String) {
+        tediArea.insertText(importPosition(), "import ${imp}\n")
+    }
+
+    fun importPosition(): Int {
+        val text = tediArea.text
+        var pos = 0
+
+        val pm = packagePattern.matcher(text)
+        while (pm.find()) {
+            pos = tediArea.positionOfLine(1 + tediArea.lineForPosition(pm.start()))
+        }
+
+        val im = importPattern.matcher(text)
+        while (im.find()) {
+            pos = tediArea.positionOfLine(1 + tediArea.lineForPosition(im.start()))
+        }
+        return pos
+    }
+
+    fun wordAt(event: MouseEvent): String? {
+        val pos = tediArea.positionForPoint(event.x, event.y)
+        val (line, column) = tediArea.lineColumnForPosition(pos)
+        val lineText = tediArea.paragraphs[line].text
+        // Find the word at the given point
+        val matcher = wordPattern.matcher(lineText)
+
+        while (matcher.find()) {
+            if (matcher.start() <= column && matcher.end() >= column) {
+                return lineText.substring(matcher.start(), matcher.end())
+            }
+        }
+        return null
     }
 
     fun onKeyPressed(event: KeyEvent) {
@@ -95,5 +194,11 @@ class CodeEditor {
         if (consume) {
             event.consume()
         }
+    }
+
+    companion object {
+        val wordPattern = Pattern.compile("\\b\\w+?\\b")
+        val packagePattern = Pattern.compile("^\\spackage", Pattern.MULTILINE)
+        val importPattern = Pattern.compile("^\\s*import\\s(?<IMPORT>.*)\\s", Pattern.MULTILINE)
     }
 }
